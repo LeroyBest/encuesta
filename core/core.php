47f00f324cb85ef909029b06c82175f7a6d35d17
507Developers <thebest19@gmail.com>
2017-08-31 14:50:23 -0500

    conexion nueva
    conexion

--- core/core.php
+++ core/core.php
@@ -6,9 +6,14 @@ define('DB_HOST','localhost');
 define('DB_USER','UserSurvey');
 define('DB_PASS','UserSurvey');
 define('DB_NAME','507survey');
-
+/*produccion
+define('DB_USER','solution_survey');
+define('DB_PASS','solution_survey');
+define('DB_NAME','solution_survey');
+*/
 define('HTML_DIR','html/');
 define('APP_URL','http://'. $_SERVER['SERVER_NAME'].'/demos/encuesta/');
+//define('APP_URL','http://'. $_SERVER['SERVER_NAME'].'/encuesta/');
 
 define('QUANTITY_QTS',5);
 
